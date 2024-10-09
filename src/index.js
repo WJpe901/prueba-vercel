ce634b18eec801c654ba9cffb3b0262a7f8e8a0c
Wesley Condori Mozo <jupemo20009@hotmail.com>
2024-10-08 22:55:45 -0500

    aniadido cosas como estilos

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Servir archivos estáticos como CSS o imágenes desde una carpeta "public"
+app.use(express.static("public"));
+
+// Ruta principal
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
